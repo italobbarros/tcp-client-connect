84c115672169492be79a3088ebac836b131ca572
Italo Barros <italo.barros@ativasolucoes.com.br>
2024-02-03 10:56:17 -0300

    feat: process valid addr

--- internal/arg/arg.go
+++ internal/arg/arg.go
@@ -52,18 +52,21 @@ func isValidAddr(addr string) error {
 	}
 	addrParts := strings.Split(addr, ":")
 	if len(addrParts) != 2 {
-		return fmt.Errorf("invalid address. The format deve ser IP:porta")
+		return fmt.Errorf("invalid address. The format should be <IP>:<Port> \n Ex: localhost:8080")
 	}
 
 	ip := addrParts[0]
 	port := addrParts[1]
 
 	if ip != "localhost" && net.ParseIP(ip) == nil {
-		return fmt.Errorf("Invalid IP")
+		return fmt.Errorf("Invalid IP. The IP address should be IPv4 form.\n Ex: 10.10.10.180")
 	}
-	_, err := strconv.Atoi(port)
+	v, err := strconv.Atoi(port)
 	if err != nil {
-		return fmt.Errorf("Invalid port")
+		return fmt.Errorf("Invalid port. The port should be a integer number between 1 and 65535. \n Ex: 8080")
+	}
+	if v < 1 || v > 65535 {
+		return fmt.Errorf("%d is invalid port. The port should be a integer number between 1 and 65535. \n Ex: 8080", v)
 	}
 	return nil
 }
