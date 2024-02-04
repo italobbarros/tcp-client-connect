ae14f0795a32b12b1247bbab182ff272a58697fd
Italobbarros <italobbaros@gmail.com>
2024-02-03 22:06:11 -0300

    feat: add correct build to linux

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Adicionando o diretório ao PATH
+FILE="tcpclient"
 # Criando a pasta de build se não existir
 mkdir -p ./build
 
 # Build para Linux
-GOOS=linux GOARCH=amd64 go build -o ./build/tcpclient
+GOOS=linux GOARCH=amd64 go build -o ./build/$FILE
 
 # Build para Windows
-GOOS=windows GOARCH=amd64 go build -o ./build/tcpclient.exe
+GOOS=windows GOARCH=amd64 go build -o ./build/$FILE.exe
 
 echo "Binários foram construídos e estão localizados em ./build."
+
+
+BINARY="$(pwd)/build/$FILE"
+# Adiciona a variável ao final do arquivo de perfil
+sudo cp $BINARY /usr/local/bin/
+sudo chmod +x /usr/local/bin/$FILE
+
+echo "Alterações aplicadas."
+
+
